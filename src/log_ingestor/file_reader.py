fee770073184e3a0097e98024a55c267fbd87989
d.matviienko <wergil1331@gmail.com>
2026-03-01 22:15:26 +0100

    save

--- src/log_ingestor/file_reader.py
+++ src/log_ingestor/file_reader.py
@@ -1,10 +1,13 @@
-from pathlib import Path
 import json
+from pathlib import Path
+
+from .validator import Car
+
 
 def file_read(file_path: str):
     file = Path(file_path)
     if not file.exists():
         raise ValueError(f"Wrong path {file_path}")
     with open(file, "r") as json_file:
-        data = json.load(json_file)
+        data: Car = json.load(json_file)
         print(f"{data}")
